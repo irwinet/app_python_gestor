a21eeaff2688b6d3769cbd75e2abb6f04ef369ad
Irwin Estrada <ext_iestrada@tottus.com.pe>
2023-01-11 12:36:33 -0500

    Implement menu

--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
-import os
+import helpers
+import database as db
 
 def iniciar():
     while True:
-        os.system('cls')
+        helpers.limpiar_pantalla()
         print("==========================")
         print("   Bienvenido al Gestor   ")
         print("==========================")
@@ -15,23 +16,44 @@ def iniciar():
         print("==========================")
 
         opcion = input("> ")
-        os.system('cls')
+        helpers.limpiar_pantalla()
 
         if opcion == '1':
             print("Listando los clientes...\n")
-            # TODO 
+            for cliente in db.Clientes.lista:
+                print(cliente)
+
         elif opcion == '2':
             print("Buscando un cliente...\n")
-            # TODO 
+            dni = helpers.leer_texto(3,3,'DNI (2 int y 1 char)').upper()
+            cliente = db.Clientes.buscar(dni)
+            print(cliente) if cliente else print('Cliente no encontrado')
+
         elif opcion == '3':
             print("Añadiendo un cliente...\n")
-            # TODO 
+            dni = helpers.leer_texto(3,3,'DNI (2 int y 1 char)').upper()
+            nombre = helpers.leer_texto(2,30,'Nombre (2 a 30 chars)').capitalize()
+            apellido = helpers.leer_texto(2,30,'Apellido (2 a 30 chars)').capitalize()
+            db.Clientes.crear(dni, nombre, apellido)
+            print("Cliente añadido correctamente")
+
         elif opcion == '4':
             print("Modificando un cliente...\n")
-            # TODO 
+            dni = helpers.leer_texto(3,3,'DNI (2 int y 1 char)').upper()
+            cliente = db.Clientes.buscar(dni)
+            if cliente:
+                nombre = helpers.leer_texto(2,30,f'Nombre (2 a 30 chars) [{cliente.nombre}]').capitalize()
+                apellido = helpers.leer_texto(2,30,f'Apellido (2 a 30 chars) [{cliente.apellido}]').capitalize()
+                db.Clientes.modificar(cliente.dni, nombre, apellido)
+                print("Cliente modificado correctamente")
+            else:
+                print("Cliente no encontrado")
+
         elif opcion == '5':
             print("Borrando un cliente...\n")
-            # TODO
+            dni = helpers.leer_texto(3,3,'DNI (2 int y 1 char)').upper()
+            print("Cliente borrado correctamente") if db.Clientes.borrar(dni) else print("Cliente no encontrado")
+
         elif opcion == '6':
             print("Saliendo...\n")
             break
