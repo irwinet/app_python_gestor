6339e55bb06d6dd3e54c432387269bbe8c12d310
Irwin Estrada <ext_iestrada@tottus.com.pe>
2023-01-11 11:53:50 -0500

    Finish pytest -v

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 import database as db
 
@@ -15,3 +16,22 @@ class TesDatabase(unittest.TestCase):
         cliente_inexiste = db.Clientes.buscar('99X')
         self.assertIsNotNone(cliente_existe)
         self.assertIsNone(cliente_inexiste)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('39X', 'Irwin', 'Estrada')        
+        self.assertEqual(len(db.Clientes.lista), 4)
+        self.assertEqual(nuevo_cliente.dni, '39X')
+        self.assertEqual(nuevo_cliente.nombre, 'Irwin')
+        self.assertEqual(nuevo_cliente.apellido, 'Estrada')
+
+    def test_modificar_cliente(self):
+        cliente_a_modificar = copy.copy(db.Clientes.buscar('28Z'))
+        cliente_modificado = db.Clientes.modificar('28Z', 'Mariana', 'Garcia')
+        self.assertEqual(cliente_a_modificar.nombre, 'Ana')
+        self.assertEqual(cliente_modificado.nombre, 'Mariana')
+
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.borrar('48H')
+        cliente_rebuscado = db.Clientes.buscar('48H')
+        self.assertEqual(cliente_borrado.dni, '48H')
+        self.assertIsNone(cliente_rebuscado)
