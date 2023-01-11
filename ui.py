c37a6e5dbd89f10004d7bb89d75bb63f9b6f5e4e
Irwin Estrada <ext_iestrada@tottus.com.pe>
2023-01-11 16:33:59 -0500

    Add CenterWidgetMixin

--- ui.py
+++ ui.py
@@ -1,17 +1,29 @@
 from tkinter import *
 
-class MainWindows(Tk):
+class CenterWidgetMixin:
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int(ws/2-w/2)
+        y = int(hs/2-h/2)
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+class MainWindows(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
         self.title("Gestor de Clientes")
         self.build()
+        self.center()
     
     def build(self):
         button = Button(self, text="Hola", command=self.hola)
         button.pack()
     
     def hola(self):
-        print("Hola Mundo")
+        print("Hola Mundo")    
 
 if __name__ == "__main__":
     app = MainWindows()
